60727d4d1623c78e2485b0c6104094556104000e
IordacheRobert <iordacherobertnicolae@gmail.com>
2016-04-11 16:20:32 +0300

    Response and Request filters

--- EventsPlatform/src/main/java/ro/ase/EventsPlatform/utils/RequestFilter.java
+++ EventsPlatform/src/main/java/ro/ase/EventsPlatform/utils/RequestFilter.java
@@ -3,21 +3,18 @@ package ro.ase.EventsPlatform.utils;
 import java.io.IOException;
 
 import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.container.ContainerResponseContext;
-import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.ext.Provider;
 
 @Provider
-public class RequestFilter implements ContainerResponseFilter{
+public class RequestFilter implements ContainerRequestFilter{
 
 	@Override
-	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
-			throws IOException {
+	public void filter(ContainerRequestContext requestContext) throws IOException {
 
-		System.out.println("A request was made! Keep going");
-		
-		
-		
+
+		System.out.println("------------------- Request ----------------");
+
 	}
 
 }
